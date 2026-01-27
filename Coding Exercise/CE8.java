45d4d7c3f74c36a2b916f0219ec6a3ede0130112
harshpatil4700 <harshpatil4700@gmail.com>
2026-01-27 19:03:45 +0530

    Initial commit

---
+++ Coding Exercise/CE8.java
@@ -0,0 +1,42 @@
+
+interface Machine {
+    String start();
+}
+ 
+abstract class Appliance implements Machine {
+    String name;
+ 
+    public Appliance(String name) {
+        this.name = name;
+    }
+}
+ 
+class Fan extends Appliance {
+    public Fan(String name) {
+        super(name);
+    }
+ 
+    public String start() {
+        return "Fan is running";
+    }
+}
+ 
+class WashingMachine extends Appliance {
+    public WashingMachine(String name) {
+        super(name);
+    }
+ 
+    public String start() {
+        return "Washing Machine is operating";
+    }
+}
+ 
+public class CE8 {
+    public static void main(String[] args) {
+        Machine fan = new Fan("Fan");
+        Machine washer = new WashingMachine("Washing Machine");
+ 
+        System.out.println(fan.start());
+        System.out.println(washer.start());
+    }
+}
