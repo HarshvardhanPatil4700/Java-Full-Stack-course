8880171d67d9ed43e6ba5fa7e6bfe01c9e444064
harshpatil4700 <harshpatil4700@gmail.com>
2026-02-11 22:24:51 +0530

    Second commit

--- Variables.java
+++ Variables.java
@@ -21,5 +21,16 @@ public class Variables {
         boolean res2 = b1 && b2;
         System.out.println(res2);
         System.out.println(!res2);
+
+        int a = 10;
+        int A = 15; // 'a' and 'A' are different variables as Java is case-sensitive language
+
+        // Default Values: If you choose not to assign a value during variable declaration, Java automatically assigns a default value based on the data type:
+        int x;
+        boolean b;
+        double d;
+        char c; // default value of char is '\u0000'
+        String s; //Object default value is null
+        System.out.println(x + " " + b + " " + d + " " + c + " " + s);
     }
 }
