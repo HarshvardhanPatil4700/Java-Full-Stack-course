13f74411cea9cc4b2a1095b6f90b96b10878ca36
harshpatil4700 <harshpatil4700@gmail.com>
2026-04-24 15:31:22 +0530

    Initial commit

---
+++ HibernateProject/src/main/java/com/harsh/Main.java
@@ -0,0 +1,11 @@
+package com.harsh;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setsName("Harsh");
+        s1.setRollNo(100);
+        s1.setsAge(25);
+        System.out.println(s1);
+    }
+}
